e22cd7b5821cb645fafac24b54264fe61e381d4d
Amir Saeid <amir@glgdgt.com>
2017-10-22 20:56:20 +0330

    Add volume generators

--- src/test/scala/com/gluegadget/DeploymentSpecification.scala
+++ src/test/scala/com/gluegadget/DeploymentSpecification.scala
@@ -8,11 +8,12 @@ import skuber.json.apps.format.depFormat
 import de.leanovate.swaggercheck.SwaggerChecks
 import de.leanovate.swaggercheck.schema.ValidationResultToProp._
 import de.leanovate.swaggercheck.shrinkable.CheckJsValue
-import org.scalacheck.{Arbitrary, Gen, Properties}
+import org.scalacheck.{Arbitrary, Properties}
 import org.scalacheck.Prop.forAll
-import skuber.{Container, ObjectMeta, Pod}
 
 object DeploymentSpecification extends Properties("Deployment") {
+  import SkuberGen._
+
   val swaggerCheck = SwaggerChecks(new FileInputStream(new File("swagger.json")))
   val definition: String = "io.k8s.kubernetes.pkg.apis.apps.v1beta1.Deployment"
 
@@ -30,52 +31,9 @@ object DeploymentSpecification extends Properties("Deployment") {
     implicit val arbitraryJson = Arbitrary[CheckJsValue](swaggerCheck.jsonGenerator(definition))
 
     forAll { j: CheckJsValue =>
-      val JsSuccess(_, p) = Json.parse(j.minified).validate[Deployment]
+      val JsSuccess(h, p) = Json.parse(j.minified).validate[Deployment]
 
       p.toString() == ""
     }
   }
-
-  def genMap: Gen[Map[String, String]] = for {
-    keys <- Gen.containerOf[List, String](Gen.identifier)
-    values <- Gen.containerOfN[List, String](keys.size, Gen.identifier)
-  } yield (keys zip values).toMap
-
-  implicit val objMetaArb: Arbitrary[ObjectMeta] = Arbitrary(for {
-    labels <- genMap
-    annotations <- genMap
-  } yield ObjectMeta(labels = labels, annotations = annotations))
-
-  implicit val containerArb: Arbitrary[Container] = Arbitrary(for {
-    name <- Gen.identifier
-    image <- Gen.identifier
-  } yield Container(name = name, image = image))
-
-  implicit val podSpecArb: Arbitrary[Pod.Spec] = Arbitrary(for {
-    containers <- Gen.containerOf[List, Container](Arbitrary.arbitrary[Container])
-  } yield Pod.Spec(containers = containers))
-
-  implicit val podTemplateSpecArb: Arbitrary[Pod.Template.Spec] = Arbitrary(for {
-    metadata <- Arbitrary.arbitrary[ObjectMeta]
-    spec <- Gen.option(Arbitrary.arbitrary[Pod.Spec])
-  } yield Pod.Template.Spec(metadata = metadata, spec = spec))
-
-  implicit val depSpecArb: Arbitrary[Deployment.Spec] = Arbitrary(for {
-    rs <- Gen.option(Gen.posNum[Int])
-    template <- Gen.option(Arbitrary.arbitrary[Pod.Template.Spec])
-  } yield Deployment.Spec(replicas = rs, template = template))
-
-  implicit val depStatusArb: Arbitrary[Deployment.Status] = Arbitrary(for {
-    rs    <- Gen.posNum[Int]
-    urs   <- Gen.posNum[Int]
-    ars   <- Gen.posNum[Int]
-    unars <- Gen.posNum[Int]
-    ors   <- Gen.posNum[Int]
-  } yield Deployment.Status(rs, urs, ars, unars, ors))
-
-  implicit val depArb: Arbitrary[Deployment] = Arbitrary(for {
-    spec <- Gen.option(Arbitrary.arbitrary[Deployment.Spec])
-    status <- Gen.option(Arbitrary.arbitrary[Deployment.Status])
-    metadata <- Arbitrary.arbitrary[ObjectMeta]
-  } yield Deployment(kind = "", apiVersion = "", metadata = metadata, spec = spec, status = status))
 }
