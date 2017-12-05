09d8a7a915e6ecfc8ea78fac367c9e37d76d15c2
Amir Saeid <amir@glgdgt.com>
2017-12-05 10:46:29 +0000

    Update dependencies

--- build.sbt
+++ build.sbt
@@ -2,12 +2,13 @@ import Dependencies._
 
 lazy val root = (project in file(".")).settings(inThisBuild(List(
   organization := "com.gluegadget",
-  scalaVersion := "2.11.11",
+  scalaVersion := "2.12.4",
   version      := "0.1.0-SNAPSHOT"
 )),
   name := "k8s-check",
   libraryDependencies ++= Seq(
     playWS,
+    skuber,
     jacksonCore,
     jacksonScala,
     jacksonYaml,
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,12 +1,13 @@
 import sbt._
 
 object Dependencies {
-  val jacksonVersion = "2.8.6"
+  val jacksonVersion = "2.9.2"
 
   lazy val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core" % jacksonVersion
   lazy val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % jacksonVersion
   lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion
   lazy val jacksonYaml = "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % jacksonVersion
-  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
-  lazy val playWS = "com.typesafe.play" %% "play-ws" % "2.4.8"
+  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.13.5"
+  lazy val playWS = "com.typesafe.play" %% "play-ws" % "2.6.7"
+  lazy val skuber = "io.skuber" %% "skuber" % "2.0.0-RC2"
 }
