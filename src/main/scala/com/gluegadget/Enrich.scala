5cb1c5994bbcc66c67c8683f4d6bbb64124530b9
Amir Saeid <amir@glgdgt.com>
2017-12-06 16:01:23 +0000

    Add format and maxLength based on description

---
+++ src/main/scala/com/gluegadget/Enrich.scala
@@ -0,0 +1,70 @@
+package com.gluegadget
+
+import java.io.{File, FileInputStream}
+
+import gnieh.diffson.Pointer
+import gnieh.diffson.playJson._
+import play.api.libs.json._
+
+sealed trait Identifier {
+  def name: String
+  def format: String
+  def maxLength: Int
+}
+
+case object DnsLabel extends Identifier {
+  val name = "DNS_LABEL"
+  val format = "^[a-z0-9][-a-z0-9]{0,61}[a-z0-9]$"
+  val maxLength = 63
+}
+
+case object DnsSubdomain extends Identifier {
+  val name = "DNS_SUBDOMAIN"
+  val format = ""
+  val maxLength = 253
+}
+
+case object PortName extends Identifier {
+  val name = "PORT_NAME"
+  val format = ""
+  val maxLength = 15
+}
+
+case class PathIdentifier(path: JsPath, identifier: Identifier)
+
+object Enrich {
+  def findPaths(str: String, path: JsPath, value: JsValue, acc: List[JsPath] = List.empty[JsPath]): List[JsPath] = {
+    value match {
+      case o: JsObject =>
+        o.fields.flatMap { e =>
+          acc ++ findPaths(str, path \ e._1, e._2, acc)
+        }.toList
+      case JsString(s) if s.contains(str) => path :: acc
+      case JsArray(a) =>
+        a.zipWithIndex.flatMap { e=>
+          acc ++ findPaths(str, path(e._2), e._1, acc)
+        }.toList
+      case _ => acc
+    }
+  }
+
+  val ids: List[Identifier] = List(DnsLabel, DnsSubdomain, PortName)
+
+  def enrich(f: File): String = {
+    val is = new FileInputStream(f)
+    val json = Json.parse(is)
+
+    val pis = ids.flatMap { i =>
+      findPaths(i.name, JsPath, json).flatMap { p: JsPath =>
+        val rootPointer = p.path.init.map(_.toString.drop(1))
+        val formatPointer = Pointer(rootPointer :+ "format" : _*)
+        val maxLengthPointer = Pointer(rootPointer :+ "maxLength" : _*)
+        List(Add(formatPointer, JsString(i.format)), Add(maxLengthPointer, JsNumber(i.maxLength)))
+      }
+    }
+
+    val patch = JsonPatch(pis)
+
+    patch(json).toString
+  }
+}
--- src/test/scala/com/gluegadget/DeploymentSpecification.scala
+++ src/test/scala/com/gluegadget/DeploymentSpecification.scala
@@ -1,11 +1,12 @@
 package com.gluegadget
 
-import java.io.{File, FileInputStream}
+import java.io.File
 
 import play.api.libs.json.{JsSuccess, Json}
 import skuber.apps.Deployment
 import skuber.json.apps.format.depFormat
 import de.leanovate.swaggercheck.SwaggerChecks
+import de.leanovate.swaggercheck.schema.SwaggerAPI
 import de.leanovate.swaggercheck.schema.ValidationResultToProp._
 import de.leanovate.swaggercheck.shrinkable.CheckJsValue
 import org.scalacheck.{Arbitrary, Properties}
@@ -14,7 +15,10 @@ import org.scalacheck.Prop.forAll
 object DeploymentSpecification extends Properties("Deployment") {
   import SkuberGen._
 
-  val swaggerCheck = SwaggerChecks(new FileInputStream(new File("swagger.json")))
+  val swagger = new File("swagger.json")
+  val enrichedSwagger: String = Enrich.enrich(swagger)
+
+  val swaggerCheck = SwaggerChecks(SwaggerAPI.parse(enrichedSwagger), maxItems = 1000)
   val definition: String = "io.k8s.kubernetes.pkg.apis.apps.v1beta1.Deployment"
 
   property("Verifier") = {
