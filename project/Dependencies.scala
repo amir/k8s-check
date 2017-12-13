b77a8fdeba9bc3a5472b72fe0278bf5838afe9c3
Amir Saeid <amir@glgdgt.com>
2017-12-13 19:56:02 +0000

    Add more Generators

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,9 +7,9 @@ object Dependencies {
   lazy val jacksonDatabind = "com.fasterxml.jackson.core" % "jackson-databind" % jacksonVersion
   lazy val jacksonScala = "com.fasterxml.jackson.module" %% "jackson-module-scala" % jacksonVersion
   lazy val jacksonYaml = "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % jacksonVersion
-  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.13.5"
   lazy val playWS = "com.typesafe.play" %% "play-ws" % "2.6.7"
   lazy val skuber = "io.skuber" %% "skuber" % "2.0.0-RC2"
   lazy val diffson = "org.gnieh" %% f"diffson-play-json" % "2.2.4"
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.0-SNAP9"
+  lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.13.5" % "test"
+  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.0-SNAP9" % "test"
 }
--- src/test/scala/com/gluegadget/SkuberGen.scala
+++ src/test/scala/com/gluegadget/SkuberGen.scala
@@ -1,18 +1,84 @@
 package com.gluegadget
 
-import org.scalatest.prop.{Generator, Randomizer, SizeParam}
+import org.scalatest.prop.{CommonGenerators, Generator, Randomizer, SizeParam}
 import skuber.Volume
 
 object SkuberGen {
-  implicit val volumeGenerator: Generator[Volume] = (szp: SizeParam, edges: List[Volume], rnd: Randomizer) => {
-    edges match {
-      case head :: tail => (head, tail, rnd)
-      case _ =>
-        val (nv, _, nr) = Generator.stringGenerator.next(szp, Nil, rnd)
-        val v = new Volume(name = nv, source = Volume.EmptyDir())
+  import CommonGenerators._
 
-        (v, Nil, nr)
+  val storageMediumGenerator: Generator[Volume.StorageMedium] = (szp: SizeParam, edges: List[Volume.StorageMedium], rnd: Randomizer) => {
+    edges match {
+      case head :: tail =>
+        (head, tail, rnd)
+      case _ =>
+        val storageMediums = List[Volume.StorageMedium](Volume.DefaultStorageMedium, Volume.MemoryStorageMedium)
+        val (nextInt, nextRandomizer) = rnd.chooseInt(0, 1)
+        val medium = storageMediums(nextInt)
+
+        (medium, Nil, nextRandomizer)
     }
   }
+
+  implicit val keyToPathGenerator: Generator[Volume.KeyToPath] = for {
+    key <- strings
+    path <- strings
+  } yield Volume.KeyToPath(key, path)
+
+  val emptyDirGenerator: Generator[Volume.EmptyDir] = for {
+    medium <- storageMediumGenerator
+  } yield Volume.EmptyDir(medium)
+
+  val optionalStrings = Generator.optionGenerator[String]
+  val listKeyPath = Generator.listGenerator[Volume.KeyToPath]
+
+  val gitRepoGenerator: Generator[Volume.GitRepo] = for {
+    repository <- strings
+    revision <- optionalStrings
+  } yield Volume.GitRepo(repository, revision)
+
+  val secretGenerator: Generator[Volume.Secret] = for {
+    secretName <- strings
+    items <- Generator.optionGenerator(listKeyPath)
+  } yield Volume.Secret(secretName, items)
+
+  val resourceFieldSelector: Generator[Volume.ResourceFieldSelector] = for {
+    containerName <- Generator.optionGenerator(strings)
+    resource <- strings
+  } yield Volume.ResourceFieldSelector(containerName, None, resource)
+
+  val objectFieldSelector: Generator[Volume.ObjectFieldSelector] = for {
+    apiVersion <- strings
+    fieldPath <- strings
+  } yield Volume.ObjectFieldSelector(apiVersion, fieldPath)
+
+  val downwardApiVolumeFile: Generator[Volume.DownwardApiVolumeFile] = for {
+    fieldRef <- objectFieldSelector
+    mode <- Generator.optionGenerator(ints)
+    path <- strings
+    resourceFieldRef <- Generator.optionGenerator(resourceFieldSelector)
+  } yield Volume.DownwardApiVolumeFile(fieldRef, mode, path, resourceFieldRef)
+
+  val configMapVolumeSource: Generator[Volume.ConfigMapVolumeSource] = for {
+    name <- strings
+    items <- listKeyPath
+  } yield Volume.ConfigMapVolumeSource(name, items)
+
+  val downwardApiVolumeSource: Generator[Volume.DownwardApiVolumeSource] = for {
+    defaultMode <- Generator.optionGenerator(ints)
+    items <- Generator.listGenerator(downwardApiVolumeFile)
+  } yield Volume.DownwardApiVolumeSource(defaultMode, items)
+
+  val persistentVolumeClaimRef: Generator[Volume.PersistentVolumeClaimRef] = for {
+    claimName <- strings
+    readOnly <- Generator.intGenerator.map(_ % 2 == 0)
+  } yield Volume.PersistentVolumeClaimRef(claimName, readOnly)
+
+  val volumeSourceGenerator: Generator[Volume.Source] = evenly(emptyDirGenerator, gitRepoGenerator, secretGenerator,
+    downwardApiVolumeSource, configMapVolumeSource, persistentVolumeClaimRef)
+
+  implicit val volumeGenerator: Generator[Volume] = for {
+    name <- strings
+    source <- volumeSourceGenerator
+  } yield Volume(name, source)
 }
 
